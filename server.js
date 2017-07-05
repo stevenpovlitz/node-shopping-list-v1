dd95d19132b6f74d06dcf29560c66fa4eb7c810a
Steven <steven.povlitz@gmail.com>
2017-07-04 20:37:01 -0400

    added recipies interaction

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,18 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// make some test data for Recipies
+Recipes.create("Sunny Side Up", ["egg", "bacon"]);
+Recipes.create("Pan seared fish", ["halibut", "green beans", "lemon"]);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
